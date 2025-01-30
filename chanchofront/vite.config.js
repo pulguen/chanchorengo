e99c4d5524b75f294647ec7f8e2303ae7033b5d8
Ricardo Rojo <109480447+pulguen@users.noreply.github.com>
2025-01-30 20:44:54 -0300

    corregion rutas para despliegue

--- chanchofront/vite.config.js
+++ chanchofront/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Asegura que la app funcione en subdirectorios en producción
+  build: {
+    outDir: 'dist', // Directorio de salida
+  },
+});
+
